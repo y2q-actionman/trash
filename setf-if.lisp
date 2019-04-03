e1fe4791076a6fded45833ade71c431ff8a8d859
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-04-04 03:10:54 +0900

    added memo

---
+++ setf-if.lisp
@@ -0,0 +1,9 @@
+(in-package :cl-user)
+
+;; 以下を返せばよい
+
+;; 1. Cond (condの値を見て if のどっちかの値を計算する式、はいらない）
+;; 2. Cond式 （condの値を見てどっちかの式を返す式、はいらない）
+;; 3. そのまま
+;; 4. Condの値を見て、どっちかの reader functionを返す
+;; 5. Condの値を見て、どっちかの writer function を返す
